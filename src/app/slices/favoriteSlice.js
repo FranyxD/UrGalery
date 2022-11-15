faff847c57c23fd95877a3b52ace1b37e037c5b7
FranyxD <Franyx2001@gmail.com>
2022-11-15 17:50:17 +0100

    pendiente... formulario 2 inputs

--- src/app/slices/favoriteSlice.js
+++ src/app/slices/favoriteSlice.js
@@ -1,39 +1,38 @@
 import { createSlice, current } from "@reduxjs/toolkit";
 
-
-
 export const favoriteSlice = createSlice({
-  name: 'favorites',
+  name: "favorites",
   initialState: JSON.parse(localStorage.getItem("reduxState")) || [],
   reducers: {
     actionAddToFavorites: (state, action) => {
       const dateSave = new Date();
-      console.log(dateSave.getTime())
-     //const fecha = `${dateSave.getFullYear()}-${dateSave.getMonth()}-${dateSave.getDate()}`
-    //const fecha = dateSave.getMilliseconds();
-      state.push({...action.payload, fecha: dateSave.getTime()})
-      console.log(current(state))
+      console.log(dateSave.getTime());
+      //const fecha = `${dateSave.getFullYear()}-${dateSave.getMonth()}-${dateSave.getDate()}`
+      //const fecha = dateSave.getMilliseconds();
+      state.push({ ...action.payload, fecha: dateSave.getTime() });
+      console.log(current(state));
     },
-      actionRemoveToFavorites: (state, action) => {
-        //console.log('favorites state: ', current(state))
-        console.log('//action: ', action.payload)
-        return state.filter((item) => item.id !== action.payload)
-      },
-      actionEditImag: (state, action) => {
-        console.log(current(state))
-        return state.map(item => {
-          console.log('id payload: ', action.payload.id)
-          console.log('action.payload: ' + action.payload.description)
-          console.log('log item: ', item)
-          if(item.id === action.payload.id)
-            return {...item, description: action.payload.description};
-            
-          return item;
-        })
-      }
-  },
-  
-})
+    actionRemoveToFavorites: (state, action) => {
+      //console.log('favorites state: ', current(state))
+      console.log("//action: ", action.payload);
+      return state.filter((item) => item.id !== action.payload);
+    },
+    actionEditImag: (state, action) => {
+      console.log(current(state));
+      return state.map((item) => {
+        if (item.id === action.payload.id)
+          return {
+            ...item,
+            description: action.payload.description,
+            alt_description: action.payload.alt_description,
+          };
 
-export const {actionAddToFavorites, actionRemoveToFavorites, actionEditImag} = favoriteSlice.actions;
+        return item;
+      });
+    },
+  },
+});
+
+export const { actionAddToFavorites, actionRemoveToFavorites, actionEditImag } =
+  favoriteSlice.actions;
 export default favoriteSlice.reducer;
